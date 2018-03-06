c2a759fe256fe17498fcd6051986b704a3e03639
wangbaotian <wbt1022@sina.com>
2018-03-06 14:02:02 +0800

    Initial Commit

---
+++ 多边形运动/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  多边形运动
+//
+//  Created by Burt on 2018/3/6.
+//  Copyright © 2018年 com.uqiauto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 多边形运动/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  多边形运动
+//
+//  Created by Burt on 2018/3/6.
+//  Copyright © 2018年 com.uqiauto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
